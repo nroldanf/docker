5354ee3f78868c074805a181d543f69658979691
nroldanf <nicolas.roldan@mail.escuelaing.edu.co>
2021-08-31 21:12:29 -0500

    fix format on bash script

--- lambda_layers/lambda_layer.sh
+++ lambda_layers/lambda_layer.sh
@@ -6,13 +6,14 @@ DIRECTORY="python/lib/python$1/site-packages/"
 DOCKER_IMAGE="lambci/lambda:build-python$1"
 # Make a directory where to save the packages
 mkdir -p $DIRECTORY
-# cd $DIRECTORY
+
 # Pull docker image corresponding to the python runtime
 echo "Pulling the image..."
 docker pull $DOCKER_IMAGE
 # Run pip install inside the docker container
 echo "Installing python dependencies..."
 docker run --rm -it -v $(pwd):/var/task $DOCKER_IMAGE pip install -r requirements.txt $2 -t $DIRECTORY
+
 # Remove info directories
 echo "Removing unnecessary files and directories..."
 find $DIRECTORY -name "*-info" -type d -exec rm -rdf {} +
@@ -22,6 +23,7 @@ find $DIRECTORY -name "tests" -type d -exec rm -rdf {} +
 rm -rdf $DIRECTORY/boto3/
 rm -rdf $DIRECTORY/botocore/
 echo "Compressing final package..."
+
 # Compress with zip the resulting directory
 zip -rq "package.zip" "python/"
 # # Remove the directory
